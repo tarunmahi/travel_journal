504afadc761473ccdd7ecd346fc51b02958bbc22
tarunmahi <tarunmahendra007@gmail.com>
2022-06-21 00:10:48 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactDOM } from "react";
 import Nav from "./components/Nav"
 import Main from "./components/Main"
 import data from "./data/data"
--- src/data/data.js
+++ src/data/data.js
@@ -6,7 +6,7 @@ export default [
         image:"mount.jpg",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021 - 24 Jan, 2021",
-        endDate: "",
+       
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
